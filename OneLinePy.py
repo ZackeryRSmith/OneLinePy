dd550355621308a1723e0752b847fd0c10ae651c
Zackery .R. Smith <72983221+ZackeryRSmith@users.noreply.github.com>
2022-03-14 08:34:56 -0400

    Fixed a dumb mistake

--- OneLinePy.py
+++ OneLinePy.py
@@ -3,7 +3,6 @@
 # Simple script that converts code into one line 
 
 with open("code.py", "r") as code_file:
-    code = code_file.read()
-    code = bytes(code, 'utf-8')
+    code = bytes(code_file.read(), 'utf-8')
 with open("out.py", "w+") as out_file:
     out_file.write("exec("+str(code)+")")
