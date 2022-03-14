315d15644e515cbd1d1cc1e652dcd3a0970f0227
Zackery .R. Smith <72983221+ZackeryRSmith@users.noreply.github.com>
2022-03-14 08:37:17 -0400

    Even smaller

--- OneLinePy.py
+++ OneLinePy.py
@@ -2,7 +2,5 @@
 # -*- coding: utf-8 -*-
 # Simple script that converts code into one line 
 
-with open("code.py", "r") as code_file:
-    code = bytes(code_file.read(), 'utf-8')
-with open("out.py", "w+") as out_file:
-    out_file.write("exec("+str(code)+")")
+with open("code.py", "r") as code_file: code = bytes(code_file.read(), 'utf-8')
+with open("out.py", "w+") as out_file: out_file.write("exec("+str(code)+")")
