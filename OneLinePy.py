a08e0bf341102238652a1ccd8411ce86d498f208
Zackery .R. Smith <72983221+ZackeryRSmith@users.noreply.github.com>
2022-03-14 08:47:32 -0400

    Comment fix

--- OneLinePy.py
+++ OneLinePy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# One line script that converts code into one line                                
+# A one line script that converts python code into one line... How useful                                
                                                                                   
 with open("code.py", "r") as code_file, open("out.py", "w+") as out_file: out_file.write("exec("+str(bytes(code_file.read(), 'utf-8'))+")")
