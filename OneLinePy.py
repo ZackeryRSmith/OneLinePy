abba9b9906fbd2099bb9bdf5d212506c0b0d6ced
Zackery .R. Smith <72983221+ZackeryRSmith@users.noreply.github.com>
2022-03-14 08:44:28 -0400

    Now one line

--- OneLinePy.py
+++ OneLinePy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Simple script that converts code into one line 
-
-with open("code.py", "r") as code_file: code = bytes(code_file.read(), 'utf-8')
-with open("out.py", "w+") as out_file: out_file.write("exec("+str(code)+")")
+# One line script that converts code into one line                                
+                                                                                  
+with open("code.py", "r") as code_file, open("out.py", "w+") as out_file: out_file.write("exec("+str(bytes(code_file.read(), 'utf-8'))+")")
