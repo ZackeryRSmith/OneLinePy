1b21a5aae45064d617462e7db94dbc37686be60c
Zackery .R. Smith <72983221+ZackeryRSmith@users.noreply.github.com>
2022-03-25 06:15:39 -0400

    Shorten variable names

--- OneLinePy.py
+++ OneLinePy.py
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 # A one line script that converts python code into one line... How useful                                
                                                                                   
-with open("code.py", "r") as code_file, open("out.py", "w+") as out_file: out_file.write("exec("+str(bytes(code_file.read(), 'utf-8'))+")")
+with open("code.py", "r") as c, open("out.py", "w+") as o: o.write("exec("+str(bytes(c.read(), 'utf-8'))+")")
